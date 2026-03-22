58eebd9f8421749282110d29ebb97e915ee1c4b4
Shreejit29 <udekarp007@gmail.com>
2026-03-22 14:05:03 +0530

    Update differential voltage dv dq.py

--- Advanced degradation modeling/differential voltage dv dq.py
+++ Advanced degradation modeling/differential voltage dv dq.py
@@ -6,45 +6,55 @@ import os
 from scipy.signal import savgol_filter
 
 
-# PATH
-
-data_folder = "data/raw/"
+# PATHS
+data_folder ="data/Processed/"
 
 files = glob(os.path.join(data_folder, "*.xlsx"))
 
--
-# SELECTED CYCLES
+print("Files found:", files)
 
-cycles_to_plot = [1, 50, 99]
+# SETTINGS
+cycles_to_plot = [3, 50, 98]
 
--
-# FUNCTION: dV/dQ
 
+# FUNCTION
 def compute_dVdQ(df_cycle):
 
-    df_cycle = df_cycle[df_cycle['Current(A)'] < 0]
+    # keep only discharge
+    df_cycle = df_cycle[df_cycle['Current(A)'] < -0.05]
+
+    # keep increasing capacity
+    df_cycle = df_cycle[df_cycle['Capacity(Ah)'].diff().fillna(0) > 0]
+
     df_cycle = df_cycle.sort_values('Capacity(Ah)')
 
     V = df_cycle['Voltage(V)'].values
     Q = df_cycle['Capacity(Ah)'].values
 
-    if len(V) < 10:
+    if len(V) < 20:
         return None, None
 
     dV = np.diff(V)
     dQ = np.diff(Q)
 
-    dQ[dQ == 0] = 1e-6
+    valid = np.abs(dQ) > 1e-4
+    dV = dV[valid]
+    dQ = dQ[valid]
+
+    if len(dV) < 10:
+        return None, None
 
     dVdQ = dV / dQ
+    dVdQ = np.clip(dVdQ, -10, 10)
 
+    # smooth
     if len(dVdQ) > 11:
         dVdQ = savgol_filter(dVdQ, 11, 3)
 
     Q_mid = (Q[:-1] + Q[1:]) / 2
+    Q_mid = Q_mid[valid]
 
     return Q_mid, dVdQ
-
 
 
 # SEPARATE PLOTS
@@ -54,7 +64,7 @@ for file in files:
     print(f"\nProcessing: {file}")
 
     df = pd.read_excel(file)
-    name = os.path.basename(file)
+    name = os.path.basename(file).replace(".xlsx", "")
 
     plt.figure(figsize=(8,5))
 
@@ -63,7 +73,6 @@ for file in files:
         cycle_data = df[df['Cycle_Index'] == c]
 
         if len(cycle_data) == 0:
-            print(f"Cycle {c} not found in {name}")
             continue
 
         Q_mid, dVdQ = compute_dVdQ(cycle_data)
@@ -75,22 +84,21 @@ for file in files:
 
     plt.xlabel("Capacity (Ah)")
     plt.ylabel("dV/dQ")
-    plt.title(f"dV/dQ (RAW) - {name}")
+    plt.title(f"{name} - dV/dQ")
     plt.legend()
-    plt.grid(True)
+    plt.grid()
 
     plt.show()
 
 
-
-#  COMBINED PLOT
+# COMBINED PLOT
 
 plt.figure(figsize=(8,5))
 
 for file in files:
 
     df = pd.read_excel(file)
-    name = os.path.basename(file)
+    name = os.path.basename(file).replace(".xlsx", "")
 
     cycle_data = df[df['Cycle_Index'] == 2]
 
@@ -108,6 +116,7 @@ plt.xlabel("Capacity (Ah)")
 plt.ylabel("dV/dQ")
 plt.title("dV/dQ Comparison (Cycle 2)")
 plt.legend()
-plt.grid(True)
+plt.grid()
 
 plt.show()
+
